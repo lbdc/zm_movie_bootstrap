7f1fe553e6381fae1f808106ceea636f8fe06c02
camera <camera@>
2016-03-21 21:16:34 -0400

    Added

---
+++ zm_movie_functions.php
@@ -0,0 +1,187 @@
+<?php
+//
+// php functions used for website
+//
+function Load_Camera()
+{
+	// Read from etc/zm/zm.conf (ubuntu) or etc/zm.conf (centos)
+	//
+	if(file_exists("/etc/zm/zm.conf")) {
+		$config = parse_ini_file('/etc/zm/zm.conf');}
+	else if(file_exists("/etc/zm.conf")) {
+		$config = parse_ini_file('/etc/zm.conf');}
+	else { echo "No zoneminder configuration zm.conf found";}
+	//
+	define('ZM_HOST', $config['ZM_DB_HOST']);
+	define('ZMUSER', $config['ZM_DB_USER']);
+	define('ZMPASS', $config['ZM_DB_PASS']);
+	define('ZM_DB', $config['ZM_DB_NAME']);
+	//
+	// Loads cameras and event range
+	$con=mysqli_connect(ZM_HOST,ZMUSER, ZMPASS, ZM_DB);
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	//
+	// The query can be combined but works fine as is
+	//
+	$result = mysqli_query($con,"SELECT Id, Name, Width, Height from Monitors");
+	while($row = mysqli_fetch_assoc($result)) {
+		$mon_name[]=$row;
+	}
+	for($i = 0; $i < count($mon_name); $i++) {
+		$j = $mon_name[$i]['Id'];
+		$result = mysqli_query($con,"SELECT MonitorId, min(Starttime), max(Endtime) from Events where MonitorId=$j");
+		while($row = mysqli_fetch_assoc($result)) {
+			$mon_event[]=$row;
+		}
+	}
+	// Don't like paranthesis in variables
+	$i=0;
+	foreach($mon_event as &$name) {
+		$name['Id'] = $mon_name[$i]['Id'];
+		$name['Name'] = $mon_name[$i]['Name'];
+		$name['Size'] = $mon_name[$i]['Width'] . ':' . $mon_name[$i]['Height'];
+		$name['Starttime'] = $name['min(Starttime)'];
+		$name['Endtime'] = $name['max(Endtime)'];
+		unset($name['min(Starttime)']);
+		unset($name['max(Endtime)']);
+		$i++;
+ 	}	
+	mysqli_close($con);
+	return $mon_event;
+}
+function Movie_Update() {
+	echo '
+	<div class="container">
+		<h2>ZM Movies</h2>
+		<table class="table text-center table-hover table-bordered table-condensed">
+			<colgroup>
+				<col class="col-sm-6">
+				<col class="col-sm-1">
+				<col class="col-sm-1">
+				<col class="col-sm-1">
+				<col class="col-sm-1">
+				<col class="col-sm-1">
+			</colgroup>	
+			<thread>
+				<tr>
+					<th>Movie</th>
+					<th class="text-center">MB</th>
+					<th class="text-center">min</th>
+					<th class="text-center">List</th>
+					<th class="text-center">Log</th>
+					<th class="text-center">Del</th>
+				</tr>
+			</thread>
+		<tbody>';
+// Show existing movies for download
+// Get movies from folder and store them into array with corresponding supporting file
+	$movie_path=""; //use local folder if not set
+	$myfile = fopen("zm_movie_path.txt", "r") or die("Unable to open zm_movie_path.txt");
+	$movie_path=fgets($myfile);
+	$movie_path=str_replace(array("\r", "\n"), '', $movie_path);
+	fclose($myfile);
+	foreach(glob($movie_path.'/*.{mkv,mp4}', GLOB_BRACE) as $value) {
+		$movie_files[] = pathinfo($value,PATHINFO_FILENAME); 
+		$movie_txt[]= basename($value,pathinfo($value,PATHINFO_EXTENSION))."txt";
+		$movie_log[]= basename($value,pathinfo($value,PATHINFO_EXTENSION))."log"; 
+		$movie_progress[]= basename($value,pathinfo($value,PATHINFO_EXTENSION))."progress"; 
+	}
+	echo '<div class="container" Id="movies">';
+	if(isset($movie_files)) {
+		foreach($movie_files as $key=>$value) {		
+		// Populate movie information
+		// Movie duration
+			$duration = preg_grep("(Movie_duration)", file($movie_path."/".$movie_log[$key]));
+			$duration = array_pop($duration);
+			$duration = explode(' ', $duration);
+			$duration = array_pop($duration);
+			// Movie Progress
+			$text_length = intval(exec("wc -l ".$movie_path."/".$movie_txt[$key]));
+			$progress = file($movie_path."/".$movie_progress[$key]);
+			$progress =round(intval($progress[0])/$text_length*100);
+			// Movie Status (check if it being encoded)
+			$encode = shell_exec("ps -ef | grep ".$movie_files[$key].".mp4 | grep -v grep | awk '{print $2}'");
+			$encode=explode("\n",$encode);
+			// Movie Size
+			$size=number_format(filesize($movie_path."/".$movie_files[$key].".mp4")/1000000/$progress*100,1);	
+			if($encode[0] > 0 && $progress < 100) {
+				$status = "Encoding"; }
+			else if ($encode[0] == 0 && $progress == 100) {
+					$status = "Completed"; }
+			else if ($text_length == 0) { 
+					$status = "No alarms"; }
+			else {$status = "error"; }
+		// Display movie files	
+			if($status=="Completed") {
+				echo '<tr>
+				<td class="text-left"><a href="'.$movie_path.'/'.$movie_files[$key].'.mp4">'.$movie_files[$key].'.mp4</a>
+				<div class="progress"><div class="progress-bar progress-bar-success" role="progressbar" style="width:100%">Completed</td>';
+			}
+			else if($status=="Encoding") {
+				echo '<tr>
+				<td class="text-left">'.$movie_files[$key].'.mp4
+				<div class="progress"><div class="progress-bar progress-bar" role="progressbar" style="width:'.$progress.'%">'.$progress.'%</td>';
+			}
+			else if($status=="No alarms") {
+				echo '<tr>
+				<td class="text-left">'.$movie_files[$key].'.mp4
+				<div class="progress"><div class="progress-bar progress-bar-warning" role="progressbar" style="width:100%">No Alarms</td>';	
+			}
+			else if($status=="Error") {
+				echo '<tr>
+				<td class="text-left">'.$movie_files[$key].'.mp4
+				<div class="progress"><div class="progress-bar progress-bar-danger" role="progressbar" style="width:100%">Error - See Log</td>';
+			}
+			echo '
+			<td>'.$size.'</td>
+			<td>'.$duration.'</td>
+			<td><a href="'.$movie_path.'/'.$movie_files[$key].'.txt"><span class="glyphicon glyphicon glyphicon-menu-hamburger"></a></span></td>
+			<td><a href="'.$movie_path.'/'.$movie_files[$key].'.log"><span class="glyphicon glyphicon glyphicon-comment"></a></span></td>
+			<td><button type="button" class="btn btn-default btn-sm" onclick=delkill("'.$movie_path.'/'.$movie_files[$key].'")><span class="glyphicon glyphicon glyphicon-trash"></span></button></td>
+			</tr>';
+		}
+	}
+}
+function Delete_Movie($movie) {
+	$encode = shell_exec("ps -ef | grep ".$movie.".mp4 | grep -v grep | awk '{print $2}'");
+	$encode=explode("\n",$encode);
+	echo '<script>alert(movie)</script>';
+	if($encode[0] > 0) {
+	// kill process
+		foreach($encode as $value) {
+			exec("kill $value");
+		}
+	}
+	else {
+	// deleting files
+		unlink($movie.".mp4");
+		unlink($movie.".txt");
+		unlink($movie.".log");
+		unlink($movie.".progress");
+	}
+}
+// Main php here
+// Called function from url
+if (defined('STDIN')) {
+	if (isset($argv)){
+		$function_call = $argv[1];
+	}
+} else {
+	if(isset($_GET["f"])) {
+		$function_call = $_GET["f"];
+	}
+}
+
+if($function_call == "Movie_Update") {
+	Movie_Update();
+}
+if($function_call == "Delete_Movie") {
+	Delete_Movie($_GET["movie"]);
+}
+if($function_call == "Load_Camera") {
+	Load_Camera();
+}
+	
+?>
---
+++ zm_movie_make.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+	include 'zm_movie_header.html';
+?>
+<style>
+input.style-2 {
+   width:40px;
+}
+input.style-1 {
+   width:60px;
+}
+
+</style>
+
+<script type="text/javascript" src="js/moment.js"></script>
+<script type="text/javascript" src="js/transition.js"></script>
+<script type="text/javascript" src="js/collapse.js"></script>
+<script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
+<link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css">
+
+</head>
+<body>
+<?php
+//
+// Load camera information from DB
+require "zm_movie_functions.php";
+$camera = Load_Camera();
+echo '<script> var camera = ';
+echo json_encode($camera);
+echo ';</script>';
+?>
+<div class="container" id="make_movie">
+<h2>Movies</h2>
+	<div class="table-responsive">
+		<table class="table table-hover table-bordered table-condensed">
+		<colgroup>
+			<col class="col-md-2">
+			<col class="col-md-8">
+		</colgroup>
+		<tbody>
+			<tr><td colspan=2>
+			<select class="form-control" id="sel_cam" name="sel_cam" onChange="sel_cam(this)">
+				echo '<option value="Select">Select Camera</option>';
+				<?php   
+				foreach($camera as $key=>$value) {
+					echo '<option value="'.$key.'">'.$camera[$key]["Name"].'</option>';
+				};  ?> 
+			</select></div>
+			<form role="form" name="make_movie" method="GET">
+                        <div class="form-group">
+			</td></tr>
+			<tr><td>Camera</td><td><input type="text" class="form-control" id="Camera" name="monitor" value="" readonly></input></td></tr>
+			<tr><td>CameraId</td><td><input type="text" class="form-control" id="CameraId" name="monitorId" value="" readonly></input></td></tr>
+			<tr><td>Video Start</td><td style="position: relative"><input type='text' class="form-control" name="start" id='start'/></td></tr>
+<!--			<tr><td></td><td><input type="number" class="style-1" id="year" max="2050" min="2000" step="1" value=""><input type="number" class="style-2" id="month" max="12" min="1" step="1" value=""><input type="number" class="style-2" id="day" max="31" min="1" step="1" value=""><input type="number" class="style-2" id="hour" max="23" min="0" step="1" value=""><input type="number" class="style-2" id="minute" max="59" min="0" step="1" value=""><input type="number" class="style-2" id="second" max="59" min="1" step="1" value=""></td></tr>
+-->			
+			<tr><td>Video End</td><td style="position: relative"><input type='text' class="form-control" name="end" id='end'/></td></tr>
+			<tr data-toggle="tooltip" title="Used with mocord"><td>Buffers (Sec.)</td><td><input type="number" name="Buffer" max="60" min="0" step="5" value="5"></td></tr>
+			<tr data-toggle="tooltip" title="Alarm or All Frames"><td>Frames</td><td><select name="Frames" class="form-control"> <option value="Alarm">Alarm</option> <option value="All">All</option></select></td></tr>
+
+			<tr><th colspan =2>Encoder Parameters</th></tr>
+			<tr data-toggle="tooltip" title="Quality: 0=Best 51=Worst"><td>CRF</td><td><input type="number" name="CRF" max="51" min="0" value="23"></td></tr>
+			<tr data-toggle="tooltip" title="Default: Main"><td>Profile</td><td><select name="Profile" class="form-control"><option value="Baseline">Baseline</option><option value="Main" SELECTED>Main</option><option value="High">High</option></select></td></tr>
+			<tr><td>Preset</td><td><select name="Preset" class="form-control"><option value="Veryslow">Veryslow</option><option value="Slow">Slow</option><option value="Medium">Medium</option><option value="Fast" SELECTED>Fast</option><option value="Faster">Faster</option><option value="Veryfast">Veryfast</option><option value="Superfast">Superfast</option><option value="Ultrafast">Ultrafast</option></select></td></tr>
+			<tr><td>Speed</td><td><input type="number" name="Speed" max="50" min="1" step="1" value="10"></td></tr>
+			<tr><td>Skip Frames</td><td><input type="number" name="MultiplierX" max="100" min="1" step="1" value="1"></td></tr>
+			<tr data-toggle="tooltip" title="* Camera Size"><td>Size</td><td><select name="Size" id="Size" class="form-control"><option value=""></option><option value="1920:1080">1920x1080</option><option value="1280:720">1280x720</option><option value="800:600">800x600</option><option value="640:480">640x480</option><option value="320:240">320x240</option></select></td></tr>
+			<tr><td>Filename</td><td><input type ="text" class="form-control" name="Filename" id="Filename" value=""></td></tr>
+			<tr><td>MaxTime</td><td><input type="number" name="MaxTime" max="60" min="1" step="1" value="60"></td></tr>
+
+			</div>
+		</tbody>
+		</table>
+		<button type="submit" name="mmovie" class="btn btn-primary btn-md">Make Movie</button>
+		</form>
+	</div>
+</div>
+<script type="text/javascript">
+var x;
+function sel_cam(sel) {
+	x = sel.value;
+	document.getElementById('Camera').value = camera[x]["Name"];
+	document.getElementById('CameraId').value = camera[x]["Id"];
+	document.getElementById('start').value = camera[x]["Starttime"];
+	document.getElementById('end').value = camera[x]["Endtime"];
+	document.getElementById('Size').value = camera[x]["Size"];
+	document.getElementById('Filename').value = camera[x]["Name"];
+/* Parse Date&time for future date time picker
+	var date = moment(camera[x]["Starttime"]).format('YYYY-MM-DD HH:mm:ss');
+	document.getElementById('year').value = moment(date).format('YYYY');
+	document.getElementById('month').value = moment(date).format('MM');
+	document.getElementById('day').value = moment(date).format('DD');
+	document.getElementById('hour').value = moment(date).format('HH');
+	document.getElementById('minute').value = moment(date).format('mm');
+	document.getElementById('second').value = moment(date).format('ss');
+*/
+}
+</script>
+<?php
+
+	// POST data, call script, clear GET
+$movie_path=""; //use local folder if not set
+$myfile = fopen("zm_movie_path.txt", "r") or die("Unable to open zm_movie_path.txt");
+$movie_path=fgets($myfile);
+$movie_path=str_replace(array("\r", "\n"), '', $movie_path);
+fclose($myfile);
+
+if(isset($_GET['mmovie'])) {
+	$command='/usr/bin/php zm_movie_encode.php '.$_GET["monitorId"].' "'.$_GET["start"].'" "'.$_GET["end"].'" '.$_GET["Buffer"].' '.$_GET["Speed"].' '.$_GET["MultiplierX"].' '.$_GET["Frames"].' '.$_GET["Size"].' '.$_GET["Profile"].' '.$_GET["Preset"].' '.$_GET["CRF"].' '.$_GET["MaxTime"].' '.$_GET["Filename"];
+	exec("($command) > /dev/null &");
+	unset($_GET);
+	$page=$_SERVER['PHP_SELF'];
+	echo '<script>location.href="'.$page.'";</script>';
+}
+?> 
+</body>
+</html>
